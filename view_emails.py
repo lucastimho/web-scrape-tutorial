d82772bd7d41583c4712940ff3ba0198e08addb6
lucastimho <lucas.t.ho.lh@gmail.com>
2022-04-06 14:42:53 -0500

    obtains result and email data

--- view_emails.py
+++ view_emails.py
@@ -1,6 +1,7 @@
 import email
 import imaplib
 import getpass
+from unittest import result
 
 M = imaplib.IMAP4_SSL("imap.gmail.com")
 email_user = getpass.getpass("Enter email: ")
@@ -10,3 +11,4 @@ print(M.list())
 print(M.select("inbox"))
 typ, data = M.search(None, 'Subject "New Test Python')
 email_id = data[0]
+result_data, email_data = M.fetch(email_id, '{RFC822}')
